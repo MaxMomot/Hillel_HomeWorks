2c0bce1b2839bfcf3b8118ca618749bd851e580c
MaxMomot <djackomo@ukr.net>
2023-09-05 13:38:04 +0300

    create ul>li

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log();
+const menuItems = document.querySelectorAll('.menu li');
+
+menuItems.forEach((item) => {
+    const submenu = item.querySelector('.submenu');
+    if (submenu) {
+        item.addEventListener('mouseenter', () => {
+            submenu.style.display = 'block';
+        });
+
+        item.addEventListener('mouseleave', () => {
+            submenu.style.display = 'none';
+        });
+    }
+});
