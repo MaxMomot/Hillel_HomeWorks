fd2991ca030c39b99f26cf58d5077dae35eb8efb
MaxMomot <djackomo@ukr.net>
2023-09-12 16:56:49 +0300

    1 popitka

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log();
+
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+
+button1.addEventListener("click", function () {
+    button1.classList.add("active");
+    button2.classList.remove("active");
+});
+
+button2.addEventListener("click", function () {
+    button2.classList.add("active");
+    button1.classList.remove("active");
+});
